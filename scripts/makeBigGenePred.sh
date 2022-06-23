0e242db6484fd891412dd8f59aafc577f5c86be2
SichongP <scpeng@ucdavis.edu>
2022-06-23 16:02:49 -0700

    fix: use annotated gtf

--- scripts/makeBigGenePred.sh
+++ scripts/makeBigGenePred.sh
@@ -2,8 +2,8 @@ set -e
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 OUTDIR="${SCRIPTPATH}/../hubDirectory/equCab3/bigBed/genePred"
-sed 's/MSY/chrY/g' ${SCRIPTPATH}/../data/Genes/faang_refseq_ensembl.gtf > ${SCRIPTPATH}/../data/Genes/faang_refseq_ensembl.renamed.gtf
-${SCRIPTPATH}/../tools/gtfToGenePred -genePredExt ${SCRIPTPATH}/../data/Genes/faang_refseq_ensembl.renamed.gtf ${SCRIPTPATH}/../data/Genes/faang_refseq_ensembl.genePred
+sed 's/MSY/chrY/g' ${SCRIPTPATH}/../data/Genes/faang_refseq_ensembl.annotated.gtf > ${SCRIPTPATH}/../data/Genes/faang_refseq_ensembl.annotated.renamed.gtf
+${SCRIPTPATH}/../tools/gtfToGenePred -genePredExt ${SCRIPTPATH}/../data/Genes/faang_refseq_ensembl.annotated.renamed.gtf ${SCRIPTPATH}/../data/Genes/faang_refseq_ensembl.genePred
 ${SCRIPTPATH}/../tools/genePredToBigGenePred ${SCRIPTPATH}/../data/Genes/faang_refseq_ensembl.genePred \
 ${SCRIPTPATH}/../data/Genes/faang_refseq_ensembl.bigGenePred.txt
 sort -k1,1 -k2,2n ${SCRIPTPATH}/../data/Genes/faang_refseq_ensembl.bigGenePred.txt > temp
