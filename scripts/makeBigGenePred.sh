d215e5cb28beaa43bd4ea6f698fa6ed4f3d551d5
SichongP <scpeng@ucdavis.edu>
2022-06-23 15:54:05 -0700

    script to make bb file

---
+++ scripts/makeBigGenePred.sh
@@ -0,0 +1,13 @@
+set -e
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+OUTDIR="${SCRIPTPATH}/../hubDirectory/equCab3/bigBed/genePred"
+sed 's/MSY/chrY/g' ${SCRIPTPATH}/../data/Genes/faang_refseq_ensembl.gtf > ${SCRIPTPATH}/../data/Genes/faang_refseq_ensembl.renamed.gtf
+${SCRIPTPATH}/../tools/gtfToGenePred -genePredExt ${SCRIPTPATH}/../data/Genes/faang_refseq_ensembl.renamed.gtf ${SCRIPTPATH}/../data/Genes/faang_refseq_ensembl.genePred
+${SCRIPTPATH}/../tools/genePredToBigGenePred ${SCRIPTPATH}/../data/Genes/faang_refseq_ensembl.genePred \
+${SCRIPTPATH}/../data/Genes/faang_refseq_ensembl.bigGenePred.txt
+sort -k1,1 -k2,2n ${SCRIPTPATH}/../data/Genes/faang_refseq_ensembl.bigGenePred.txt > temp
+mv temp ${SCRIPTPATH}/../data/Genes/faang_refseq_ensembl.bigGenePred.txt
+${SCRIPTPATH}/../tools/bedToBigBed -type=bed12+8 -tab -as=${SCRIPTPATH}/../data/bedsource/bigGenePred.as \
+${SCRIPTPATH}/../data/Genes/faang_refseq_ensembl.bigGenePred.txt \
+${SCRIPTPATH}/../data/equcab3.chrom.sizes $OUTDIR/faang_genePred.bb
