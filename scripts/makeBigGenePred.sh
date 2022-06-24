d4cc6202dfbb1e68272f35ed8509cb7ad5c46f49
SichongP <scpeng@ucdavis.edu>
2022-06-23 18:20:43 -0700

    fix: human readable gene names now showing up

--- scripts/makeBigGenePred.sh
+++ scripts/makeBigGenePred.sh
@@ -2,8 +2,8 @@ set -e
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 OUTDIR="${SCRIPTPATH}/../hubDirectory/equCab3/bigBed/genePred"
-sed 's/MSY/chrY/g' ${SCRIPTPATH}/../data/Genes/faang_refseq_ensembl.annotated.gtf > ${SCRIPTPATH}/../data/Genes/faang_refseq_ensembl.annotated.renamed.gtf
-${SCRIPTPATH}/../tools/gtfToGenePred -genePredExt ${SCRIPTPATH}/../data/Genes/faang_refseq_ensembl.annotated.renamed.gtf ${SCRIPTPATH}/../data/Genes/faang_refseq_ensembl.genePred
+#sed 's/MSY/chrY/g' ${SCRIPTPATH}/../data/Genes/faang_refseq_ensembl.annotated.gtf > ${SCRIPTPATH}/../data/Genes/faang_refseq_ensembl.annotated.renamed.gtf
+${SCRIPTPATH}/../tools/gff3ToGenePred -rnaNameAttr=gene_name -geneNameAttr=attr=gene_name ${SCRIPTPATH}/../data/Genes/faang_refseq_ensembl.annotated.gff3 ${SCRIPTPATH}/../data/Genes/faang_refseq_ensembl.genePred
 ${SCRIPTPATH}/../tools/genePredToBigGenePred ${SCRIPTPATH}/../data/Genes/faang_refseq_ensembl.genePred \
 ${SCRIPTPATH}/../data/Genes/faang_refseq_ensembl.bigGenePred.txt
 sort -k1,1 -k2,2n ${SCRIPTPATH}/../data/Genes/faang_refseq_ensembl.bigGenePred.txt > temp
